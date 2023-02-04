2545bab553312b9c2f0534d4aea595da495a5ae4
1233day <1233day@naver.com>
2023-02-04 19:02:40 +0900

    22강

--- src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -3,7 +3,6 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class HelloSpringApplicationTests {
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringApplicationTests.class, args);
