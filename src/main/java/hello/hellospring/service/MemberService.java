bdaae6b2608440cdd2a54e77d0fbe38f21a9e4ea
1233day <1233day@naver.com>
2023-02-05 19:31:06 +0900

    26강

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,9 +24,20 @@ public class MemberService {
     //회원가입
     public long join(Member member){
         //같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try{
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -38,7 +49,18 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try{
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+
+
     }
 
     public Optional<Member> findOne(Long memberId){
